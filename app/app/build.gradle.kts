bed1d744e77d87cf9bdf9a33ec338c94310b8803
Abel <abeln@unizar.es>
2023-11-06 20:15:37 +0100

    V 0.2 - Click the key to open the identification page

--- app/app/build.gradle.kts
+++ app/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "es.unizar.gde.puf"
         minSdk = 16
         targetSdk = 33
-        versionCode = 1
-        versionName = "0.1"
+        versionCode = 2
+        versionName = "0.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
